a5f4f6cf20c2f7724bba36555eb1511c9dcf6585
khatanasudhir <khatanasudhir786@gmail.com>
2026-03-06 18:23:20 +0530

    Configured PostgreSQL datasource and JPA settings

---
+++ src/main/java/com/khatana/ecommerce_backend/entity/Product.java
@@ -0,0 +1,15 @@
+package com.khatana.ecommerce_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+}
---
+++ src/main/java/com/khatana/ecommerce_backend/repositry/ProductRepo.java
@@ -0,0 +1,9 @@
+package com.khatana.ecommerce_backend.repositry;
+
+import com.khatana.ecommerce_backend.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Long> {
+}
